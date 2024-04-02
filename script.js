ce422f2a58ef5faeccebd44af9db245a6f4edd7b
Juariya Akter <juariyaakter97@gmail.com>
2024-04-02 13:09:39 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let colors = "red, green, black, white, blue, pink";
+{
+    color = "pink" ;
+}
+
+
+const color = ["red", "green", "black", "white", "blue", "pink"];
+document.getElementById("name").innerHTML = color;
+
+color. push("violet")
+
+color. pop("pink")
+
+console.log(color.length);
+
+
+
+
+
+var FirstColor = "pink";
+
+var FifstColor = "black";
+
+if (FifstName < LastName){
+    console.log("Give me a phone");
+}
+else{
+    console.log("Give me a phone ");
+}
